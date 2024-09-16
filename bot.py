dad06bf9e4c7abc5dfaec2db81c094d26fb22876
Николай <58044126+GameMorg@users.noreply.github.com>
2024-09-16 21:57:16 +0500

    alpha_001

--- bot.py
+++ bot.py
@@ -21,9 +21,25 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
+# @bot.message_handler(func=lambda message: True)
+# def echo_message(message):
+#     bot.reply_to(message, message.text)
+#     bot.send_message(message.chat.id, "Привет1")
+
+def f(message):
+    if (message.text.splitlines()[0] == 'Мерки') or (message.text.splitlines()[0] == 'мерки'):
+        return message
+    bot.send_message(message.chat.id, "Для расчетов напишите Мерки в начале сообщения")
+
+@bot.message_handler(func= f)
+def myFunk(message):
+    if message.text != "Falce":
+        bot.send_message(message.chat.id, "Мерки приняты")
+        answer = message.text.splitlines()
+        counMeasure = len(answer) - 1
+        bot.send_message(message.chat.id, "Количество мерок: " + str(counMeasure))
+
+
 
 
 
